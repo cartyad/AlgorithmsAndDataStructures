719c6b1a23ac9234eca2e1e0448792897fe564b7
cartyad <cartyad@tcd.ie>
2020-02-09 21:19:37 +0000

    Update SortComparison.java
    Finished merge sort recursive version

--- SortComparison/src/SortComparison.java
+++ SortComparison/src/SortComparison.java
@@ -148,10 +148,39 @@
      * @param a: An unsorted array of doubles.
      * @return after the method returns, the array must be in ascending sorted order.
      */
-    static double[] mergeSortRecursive (double a[]) {
-                  
+    static double[] mergeSortRecursive (double a[]) { //Top-Down
+    	double [] aux = new double[a.length];
+    	sort(a,aux,0,a.length-1); 
     }  
-    
+    private static void sort(double[]a, double[]aux, int lo, int hi)
+    {
+    	if(hi<=lo) return;
+    	int mid = lo + (hi-lo)/2;
+    	sort(a,aux,lo,mid);
+    	sort(a,aux,mid+1,hi);
+    	merge(a,aux,lo,mid,hi);
+    }
+    private static void merge(double[]a,double[]aux,int lo,int mid, int hi )
+    {
+    	for(int k = lo; k<=hi; k++)
+    	{
+    		aux[k]=a[k];
+    	}
+    	int i=lo,j=mid+1;
+    	for(int k=lo;k<=hi; k++)
+    	{
+    		if(i>mid) a[k]=aux[j++];
+    		else if(j>hi) a[k] = aux[i++];
+    		else if(less(aux[j],aux[i])) a[k]=aux[j++];
+    		else a[k]=aux[i++];
+    	}	
+    }
+    public static boolean less(double a, double b)
+	{
+		boolean isLess=false;
+		if(a<b) isLess=true;
+		return isLess;
+	}
     //end mergeSortRecursive
     	
     public static void main(String[] args) {
