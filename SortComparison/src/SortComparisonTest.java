492ce20bd1253f623368db65501bee1573de545f
cartyad <cartyad@tcd.ie>
2020-02-17 19:18:57 +0000

    Update SortComparisonTest.java
    Tests Completed for insertion sort

--- SortComparison/src/SortComparisonTest.java
+++ SortComparison/src/SortComparisonTest.java
@@ -14,6 +14,7 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class SortComparisonTest
 {
+	private static final double DELTA = 1e-15;
     //~ Constructor ........................................................
     @Test
     public void testConstructor()
@@ -62,6 +63,24 @@ public class SortComparisonTest
     {
     	double [] a = null;
     	assertNull(SortComparison.mergeSortRecursive(a));
+    }
+    
+    
+    @Test
+    public void testInsertionSort()
+    {
+    	double [] a = {0.5, 3.0, 0.1, 6.0, 2.0};
+    	double [] aSorted = {0.1, 0.5, 2.0, 3.0, 6.0};
+    	double [] aNegative = {-0.28, -0.65, -0.32, 0.3, -0.18, -1.0, -0.05};
+    	double [] aNegativeSorted = {-1.0, -0.65, -0.32,-0.28, -0.18, -0.05, 0.3};
+    	double [] aSingle = {0.1};
+    	double [] aSingleSorted = {0.1};
+    	double [] b = SortComparison.insertionSort(a);
+    	double [] bNegative = SortComparison.insertionSort(aNegative);
+    	double [] bSingle = SortComparison.insertionSort(aSingle);
+    	assertArrayEquals(b, aSorted, DELTA);
+    	assertArrayEquals(bNegative, aNegativeSorted, DELTA);
+    	assertArrayEquals(bSingle, aSingleSorted, DELTA);
     }
 
 
